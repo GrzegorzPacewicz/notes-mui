260a54cf7b522fa74aaad0ea87ff29a7c580b6ca
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2023-07-14 13:07:01 +0200

    Typography, Container and Button added

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,9 +1,28 @@
+import { Button, Container, Typography } from "@mui/material";
 
 const Notes = () => {
     return (
-        <div>
-            Notes page
-        </div>
+        <Container>
+
+            <Typography
+                variant="h6"
+                component="h2"
+                color="textSecondary"
+                gutterBottom
+            >
+                Create a New Note
+            </Typography>
+
+            <Button
+                onClick={() => console.log('Clicked!')}
+                type="submit"
+                color="secondary"
+                variant="contained"
+            >
+                Submit
+            </Button>
+
+        </Container>
     );
 };
 
