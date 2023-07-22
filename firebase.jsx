da4ecdbfe7cbf93f8e3dc81dcf29364381876e67
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-07-22 20:21:27 +0200

    firebase.jsx installed

---
+++ firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBEN6VycNuJNm8Z11FWXp10sWQxIqzs0eg",
+    authDomain: "grzegorz-notes.firebaseapp.com",
+    projectId: "grzegorz-notes",
+    storageBucket: "grzegorz-notes.appspot.com",
+    messagingSenderId: "1020471361052",
+    appId: "1:1020471361052:web:2777702b6610c9d13529d9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
